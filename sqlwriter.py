cae244f684a2486b5a4e7f958b556cdaa95d2fd0
Ugur Akdogan <57003166+fatalerrorist@users.noreply.github.com>
2020-03-13 15:31:38 +0300

    Add files via upload

--- sqlwriter.py
+++ sqlwriter.py
@@ -1,22 +1,45 @@
 import requests
 import json
-import psycopg2
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-#CONNECTION DB
-con=psycopg2.connect(
-       host="127.0.0.1",
-       database="fortest",
-       user="postgres",
-       password="741852963ugur")
+#FLASKSETUP
+app=Flask(__name__)
+#sqlite is working
+#app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
-cursor= con.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS ads(ad_id uuid,currency varchar,Name varchar,developer_id varchar,total_land INT,total_units smallint,city_id varchar,latitude point,longitude point,max_unit_price numeric,min_unit_price numeric,district_id varchar,rt_score INT)")
-con.commit()
+#PostgreSQL isn't working
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:741852963Ugur@localhost:52150/fortest'
 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db=SQLAlchemy(app)
+
+class ads(db.Model):
+    id=db.Column('id', db.Integer,primary_key=True)
+    ad_id=db.Column(db.Integer)
+    currency=db.Column(db.String) 
+    Name=db.Column(db.String) 
+    developer_id=db.Column(db.Integer)
+    total_land=db.Column(db.Integer)
+    total_units=db.Column(db.Integer)
+    city_id=db.Column(db.String)
+    latitude=db.Column(db.Integer)
+    longitude=db.Column(db.Integer)
+    max_unit_price=db.Column(db.Integer)
+    min_unit_price=db.Column(db.Integer)
+    district_id=db.Column(db.String)
+    rt_score=db.Column(db.Integer)
+    
+
+db.create_all()
+
+
+
+#PAGELIMITER
 limit=int(input("Choose page limit="))
 
 page_id=1
-
+#PARSING
 while page_id<=limit:
     
     url = "https://www.hurriyetemlak.com/projeler/estate/estateforbounds"
@@ -29,13 +52,13 @@ while page_id<=limit:
     page=json.loads(response.content)
     data={}
 
-
+    #AD PARSING
     ad=0
     while ad<=15:
        edited_page=page["ads"]
        json_ad=edited_page[ad]
        
-       
+       #AD CONFIG
        ad_id = json_ad["Id"]
        currency = json_ad["CurrentCurrency"]
        Name = json_ad["NameSeo"]
@@ -49,14 +72,8 @@ while page_id<=limit:
        min_unit_price = json_ad["MinPrice"]
        district_id = json_ad["DistrictName"]
        rt_score = json_ad["Quality"]
-       
-       cursor.execute(f"INSERT INTO ads Values({ad_id},{currency},{Name},{developer_id},{total_land},{total_units},{city_id},{latitude},{longitude},{max_unit_price},{min_unit_price},{district_id},{rt_score})")
-       con.commit()
-       
+    
        
        ad+=1
 
     page_id+=1
-
-cursor.close()
-con.close()
